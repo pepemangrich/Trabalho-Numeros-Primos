9efc500e7f6129acb4fc470e6853ad09b44ff48f
Pedro Carvalho Mangrich Labouriau <pepemangrich@gmail.com>
2016-07-01 11:04:43 -0300

    Trabalho Números  Primos
    Trio: Edilson José dos Santos Júnior, Gustavo Amaral de Souza Melo e
    Pedro Carvalho Mangrich Labouriau.
    Turma: 2003

---
+++ ConsoleApplication1/Primos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Primos
+    {
+        public List<int> numeros = new List<int>();
+        public int limite = 20000;
+
+        public void iniciaLista()
+        {
+            for (int i = 2; i <= limite; i++)
+            {
+                numeros.Add(i);
+            }
+        }
+
+        public void removeNaoPrimos()
+        {
+            for (int i = 0; i < numeros.Count; i++) 
+            {
+                for (int j = i + 1; j < numeros.Count; j++)
+                {
+                    if (numeros.ElementAt(j) % numeros.ElementAt(i) == 0)
+                    {
+                        numeros.RemoveAt(j);
+                      
+                    }
+                }
+                }
+        }
+
+
+
+        public void imprimeNumeros()
+        {
+           foreach (var n in numeros)
+            {
+                Console.Write(n.ToString() + "; ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
